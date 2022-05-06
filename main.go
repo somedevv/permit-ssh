5cfe540a9163258d03b046156949948d793cba86
somedevv <somedevv@somedevv.com>
2022-05-06 18:13:31 +0200

    Added checks, basic functionality and DB

---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/boltdb/bolt"
+)
+
+var (
+	user       *string
+	key        *string
+	list_users *bool
+)
+
+func init() {
+	user = flag.String("user", "", "username")
+	key = flag.String("key", "", "pub RSA key")
+	list_users = flag.Bool("list", false, "")
+}
+
+func main() {
+
+	// Open the permit.db data file in the data directory.
+	// It will be created if it doesn't exist.
+	db, err := bolt.Open("data/permit.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create bucket if it doesn't exist
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("DataBucket"))
+		if err != nil {
+			log.Fatalf("create bucket: %s", err)
+		}
+		return nil
+	})
+
+	flag.Parse()
+
+	// If true print all saved users and exit
+	// TODO: Make the print prettier
+	if *list_users == true {
+		db.View(func(tx *bolt.Tx) error {
+			// Assume bucket exists and has keys
+			b := tx.Bucket([]byte("DataBucket"))
+
+			c := b.Cursor()
+
+			for k, v := c.First(); k != nil; k, v = c.Next() {
+				fmt.Printf("USER: %s   KEY: %s\n", k, v)
+			}
+			return nil
+		})
+		return
+	}
+
+	if *user == "" && *key == "" {
+		fmt.Println("Usage: main.go")
+		flag.PrintDefaults()
+		return
+	}
+
+	// If *user and *key exist
+	if *user != "" && *key != "" {
+		db.Update(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte("DataBucket"))
+			v := b.Get([]byte(*user))
+
+			// Checks if the *key of the user equals the one stored in the db
+			if string(v) != *key {
+
+				// If the key is empty, the user didn't exist
+				if string(v) == "" {
+					fmt.Printf("Saving user...\n")
+
+				} else { // Else, the *key stored is different from the inputed one // TODO: Promt the user to confirm *key update
+					fmt.Printf("Updating user key...\n")
+				}
+
+				// The key is added to the DB and associated with the user
+				if err := b.Put([]byte(*user), []byte(*key)); err != nil {
+					log.Fatalf("Error: %s", err.Error())
+				}
+			}
+			return nil
+		})
+
+		// If *user exist but *key not
+	} else if *user != "" && *key == "" {
+		db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte("DataBucket"))
+			v := b.Get([]byte(*user))
+
+			// If the key is empty the user doesn't exist, the program quits
+			// because there is no supplied key to store
+			// TODO: Promt user if they want to add the user by providing the key insted of quitting
+			if string(v) == "" {
+				log.Fatalf("The user %s doesn't exist", *user)
+			}
+			*key = string(v)
+			return nil
+		})
+
+	}
+
+	fmt.Printf("USER: %s   KEY: %s\n", *user, *key)
+
+	defer db.Close()
+	return
+
+}
---
+++ questions.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/AlecAivazis/survey/v2"
+
+var newUser = []*survey.Question{
+	{
+		Name:      "name",
+		Prompt:    &survey.Input{Message: "What is your name?"},
+		Validate:  survey.Required,
+		Transform: survey.Title,
+	},
+	{
+		Name: "color",
+		Prompt: &survey.Select{
+			Message: "Choose a color:",
+			Options: []string{"red", "blue", "green"},
+			Default: "red",
+		},
+	},
+	{
+		Name:   "age",
+		Prompt: &survey.Input{Message: "How old are you?"},
+	},
+}
